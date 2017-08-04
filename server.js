efedef1723f49ffd5e4aafa2b17ee08686138cfa
gyanhajong <hazgyan@gmail.com>
2017-08-04 18:23:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,57 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+  title: 'Article|Gyaniswar Hajong',
+  heading: 'Article One',
+  date: 'August 4th 2017',
+  content:`  <p>
+            Hello.This is my article one.There is nothing much to say about it but it is getting fun.
+        </p>`
+};
+function createTemplate(data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    var htmlTemplate = `
+        <html>
+            <head>
+                <title>
+                    ${title}
+                </title>
+            <meta name = "viewport" content = "width-device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+            </head>
+            <body>
+                <div class = "container">
+                    <div>
+                        <a href = "/">Home</a>
+                        <hr>
+                   </div>
+                   <div>
+                        <h3>
+                            ${heading}
+                        </h3>
+                </div>
+                <div>
+                    ${date}
+                </div>
+                ${content}
+        </div>
+        </body>
+        </html>
+        
+        `;
+    return htmlTemplate;
+}        
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function (req, res) {
-   res.sendFile(path.join(__dirname,'ui','article-one.html')); 
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function(req, res) {
