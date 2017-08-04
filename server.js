9dece60487aded85b74e77e71de8499836b63b8f
gyanhajong <hazgyan@gmail.com>
2017-08-04 19:07:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,14 +81,6 @@ app.get('/:articleName',function (req, res) {
 });
 
 
-app.get('/article-two',function (req, res) {
-   res.send('Article two requested will be served here.'); 
-});
-
-app.get('/article-three',function (req, res) {
-   res.send('Article three requested will be served here.'); 
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
