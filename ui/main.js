9b0196fe2543bce3007219fc5d5c006196a6d127
gyanhajong <hazgyan@gmail.com>
2017-08-05 17:04:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,11 @@ console.log('Loaded!');
 var element = document.getElementById('te');
 
 element.innerHTML = 'This is the new content';
+
+//Move the Image
+
+var img = document.getElementById('madi');
+
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+};
