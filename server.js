0e7ff3f52ada042c8fbfa68de68f11bb7b5797f2
gyanhajong <hazgyan@gmail.com>
2017-08-05 17:56:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,11 @@ function createTemplate(data){
         `;
     return htmlTemplate;
 }        
-
+var counter = 0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -93,11 +97,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var counter = 0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
+
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
