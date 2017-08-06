8b1d5495ef860c5dc723aa05f7f8950f641b02fd
gyanhajong <hazgyan@gmail.com>
2017-08-06 10:38:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,13 +1,24 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
-    //Make a request to a counter endpoint
+    //Create a request
+    var request = new XMLHttpRequest();
+    
     
     //Captures the request and store it in a variable
+    request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE){
+            if(request.status == 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
     
-    //Render the variable in the correct span
     
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Captures the request
+    request.open('GET','http://hazgyan.imad.hasura-app.io/counter',true);
+   request.send(null);
+   
 };
