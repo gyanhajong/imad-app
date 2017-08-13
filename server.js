6830d485f6e9b5d1efd6a32bb2ed1d98f9d85e1c
Gyaniswar Hajong <hazgyan@gmail.com>
2017-08-13 06:50:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
+var Pool = require('pg').Pool;
+var config = {
+    user: 'hazgyan',
+    database: 'hazgyan',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 app.use(morgan('combined'));
 
 var articles = {
@@ -75,6 +82,21 @@ app.get('/counter', function (req, res) {
     counter=counter+1;
   res.send(counter.toString());
 });
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else {
+           res.send(JSON.stringify(result));
+       }
+   });
+   //return a response with the result
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
